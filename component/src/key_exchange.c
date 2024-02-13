799ca1589282ca522f786b223a0552d6841ad2d0
zhanhaoruan <zhanhaor@uci.edu>
2024-02-12 18:18:16 -0800

    Cleaning up

--- component/src/key_exchange.c
+++ component/src/key_exchange.c
@@ -10,27 +10,6 @@ uint8_t transmit_buffer1[MAX_I2C_MESSAGE_LEN];
 uint8_t receive_buffer1[MAX_I2C_MESSAGE_LEN];
 
 
-// void XOR_secure(unsigned char* arr1, unsigned char* arr2, int size, unsigned char* dest){
-//     /*
-//     Xor two bytes array correspondingly and write the final result to the dest array
-//     Helper function 
-
-//     Inputs: 
-//         array1 in bytes eg: { 0xDF, 0x68, 0x06, 0x80, 0x0C, 0xAA } 
-//         array2 in bytes, same size as array 1 
-//         Size: len of any array
-//         Dest: result array
-    
-//     Outputs: None
-//     */
-
-//     for (int i = 0; i < size; i++) {
-//         dest[i] = arr1[i] ^ arr2[i];
-//     }
-//     return;
-// }
-
-
 
 void sync2(char* dest, char* k2_m1){
     char cash_k2_r[18];

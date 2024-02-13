abdfcf5232fd7c389bd5a99302a404e8b2e47433
zhanhaoruan <zhanhaor@uci.edu>
2024-02-12 16:09:31 -0800

    rebuild

--- application_processor/inc/ectf_params.h
+++ application_processor/inc/ectf_params.h
@@ -4,5 +4,5 @@
 #define AP_TOKEN "0123456789abcdef"
 #define COMPONENT_IDS 0x11111124, 0x11111125
 #define COMPONENT_CNT 2
-#define AP_BOOT_MSG "Message"
+#define AP_BOOT_MSG "Test boot message"
 #endif

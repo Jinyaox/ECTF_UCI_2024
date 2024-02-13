2aea5ba1ae1cd7009240936c0067eaa7e7540150
Yintong Luo <yintol1@uci.edu>
2024-02-12 18:00:00 -0800

    fixed xor not found

--- application_processor/inc/xor.h
+++ application_processor/inc/xor.h
@@ -1,7 +1,7 @@
 #ifndef __byte_stream_XOR__
 #define __byte_stream_XOR__
 
-void XOR(unsigned char* arr1, unsigned char* arr2, int size, unsigned char* dest){
+void XOR(unsigned char* arr1, unsigned char* arr2, int size, unsigned char* dest);
     /*
     Xor two bytes array correspondingly and write the final result to the dest array
     Helper function 
@@ -14,11 +14,5 @@ void XOR(unsigned char* arr1, unsigned char* arr2, int size, unsigned char* dest
     
     Outputs: None
     */
-
-    for (int i = 0; i < size; i++) {
-        dest[i] = arr1[i] ^ arr2[i];
-    }
-    return;
-}
 
 #endif

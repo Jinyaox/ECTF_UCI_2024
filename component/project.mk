77003e9282ad3abf4e8409a236f29ec173a36236
zhanhaoruan <zhanhaor@uci.edu>
2024-02-19 18:41:19 -0800

    component clean up

--- component/project.mk
+++ component/project.mk
@@ -12,7 +12,7 @@
 # **********************************************************
 
 # Add your config here!
-#$(shell python comp_making.py)
+$(shell python comp_making.py)
 
 # This example is only compatible with the FTHR board,
 # so we override the BOARD value to hard-set it.

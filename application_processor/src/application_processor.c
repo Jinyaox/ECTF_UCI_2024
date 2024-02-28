b60373b93ee3cd6b84dba9738f031889f7e72be7
zhanhaoruan <zhanhaor@uci.edu>
2024-02-28 13:53:18 -0800

    list bug fixed

--- application_processor/src/application_processor.c
+++ application_processor/src/application_processor.c
@@ -366,6 +366,8 @@ int scan_components() {
     uint8_t receive_buffer[MAX_I2C_MESSAGE_LEN];
     uint8_t transmit_buffer[MAX_I2C_MESSAGE_LEN];
 
+    int len;
+
     // Scan scan command to each component
     for (i2c_addr_t addr = 0x8; addr < 0x78; addr++) {
         // I2C Blacklist:

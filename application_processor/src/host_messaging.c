d54d69445701882cd04805cae91cb7ea2e32d2ee
zhanhaoruan <zhanhaor@uci.edu>
2024-03-12 16:47:48 -0700

    input receive error

--- application_processor/src/host_messaging.c
+++ application_processor/src/host_messaging.c
@@ -20,7 +20,7 @@ void recv_input(const char *msg, char *buf) {
     print_debug(msg);
     fflush(0);
     print_ack();
-    fgets(buf, sizeof(buf), stdin);
+    fgets(buf, 100, stdin);
     buf[strcspn(buf, "\n")] = '\0';
     // if() TODO: need to do something here?
     puts("");
